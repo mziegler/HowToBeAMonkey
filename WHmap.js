3aeefed29595f68c6388e623886f5e202e7f80bb
Matt Ziegler <mpziegler@wisc.edu>
2014-02-26 10:39:27 -0600

    clustering for behavior points

--- WHmap.js
+++ WHmap.js
@@ -3,38 +3,82 @@ var map = L.map('map').setView([10.512, -85.366], 16);
 // base map (satelite images)
 L.tileLayer('http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
   attribution: 'Satelite images courtesy of Google',
-  maxZoom: 19
+  maxZoom: 22
 }).addTo(map);
 
 // add GPS track      
 var polyline = L.polyline(WHtrack, {color: 'blue'}).addTo(map);
 
-/*L.marker(
-    [10.512, -85.366], {
-      icon: L.divIcon({
-        className: 'leaflet-label leaflet-label-right',
-        html: '<b>Yo</b> hi what\'s going on',
-        iconSize: ['auto','auto']
-      })
-    }).addTo(map);*/
+var alternator = false;
 
-// plot behavior markers
+function alternateClass()
+{
+  alternator = !alternator;
+  if (alternator)
+    return "leaflet-label-left";
+  else
+    return "leaflet-label-right";
+}
 
+
+function clusterIcon(cluster) {
+  var childrenToShow = 5;
+
+  var children = cluster.getAllChildMarkers();
+  
+  var html = "<div class='cluster'><ul>";
+  
+  for (var i=0; i<childrenToShow; i++)
+  {
+    var markerHtml = children[i].options.icon.options.html;
+    html = html + '<li>' + markerHtml + '</li>';
+    
+  }
+  
+  html = html + '</ul>';
+  
+  if (children.length > childrenToShow)
+    html = html + ' and ' + (children.length - childrenToShow) + ' more.';
+  
+  html = html + '</div>';
+  
+  return L.divIcon({
+    className: 'leaflet-label-cluster ' + alternateClass() + '-cluster',
+    iconSize: ['auto', 'auto'],
+    html: html,
+  });
+  
+}
+
+
+// cluster layer for behavior markers
+var behaviors = new L.MarkerClusterGroup({
+  spiderfyOnMaxZoom: false,
+  iconCreateFunction: clusterIcon,
+  riseOnHover: true,
+  });
+
+
+// add behavior markers to map
 for (var i = 0; i < behaviorPoints.length; i++)
 {
   var row = behaviorPoints[i];
   
-  L.marker(
+  behaviors.addLayer(new L.marker(
     [
       row[0],
       row[1]
     ], {
       icon: L.divIcon({
-        className: 'leaflet-label leaflet-label-right',
-        html: '<b>' + row[2] + '</b> ' + row[3],
+        className: 'leaflet-label ' + alternateClass(),
+        html: '<div><b>' + row[2] + '</b> ' + row[3] + '</div>',
         iconSize: ['auto', 'auto']
       }),
       riseOnHover: true,
-    }).addTo(map);
+    })
+  );
 }
+
+map.addLayer(behaviors);
+
 
