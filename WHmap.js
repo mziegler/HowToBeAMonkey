868c6adb9de2feb944d91982f3a2f344abeb49ed
Matt Ziegler <mattpziegler@gmail.com>
2014-07-26 19:44:30 -0500

    added quote icon shadow

--- WHmap.js
+++ WHmap.js
@@ -222,9 +222,11 @@ for (var i = 0; i < textBoxes.length; i++)
 {
   var quoteIcon = L.icon({
     iconUrl: 'libraries/images/quote.png',
+    shadowUrl: 'libraries/images/quoteShadow.png',
     iconSize:     [40,40], // size of the icon
     iconAnchor:   [15 + textBoxes[i][3][0], 35 + textBoxes[i][3][1]], // point of the icon which will correspond to marker's location
-    popupAnchor:  [10, -40] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [10, -40], // point from which the popup should open relative to the iconAnchor
+    shadowAnchor: [15 + textBoxes[i][3][0], 35 + textBoxes[i][3][1]]
   });
 
   textBoxLayer.addLayer(
