c316cb2569a73ed08addd7a73beb3a78e58362c7
Matt Ziegler <mattpziegler@gmail.com>
2014-09-06 20:32:30 -0700

    re-designed donate pane

--- WHmap.js
+++ WHmap.js
@@ -55,7 +55,7 @@ $('#tab-biographies').click( function() { toggleSidePanel('#panel-biographies');
 
 // add paypal link when donate panel is opened
 $('#tab-donate').click( function() {
-  $('#paypall-wrapper:empty').html('Donate to the Capuchin Foundation via PayPal:<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="margin:5px 0 0 0;"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="MWBPPSVMGF3BY"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>');
+  $('#paypall-wrapper:empty').html('Donate to the Capuchin Foundation via PayPal:<form id="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" style="margin:15px 0 0 0;"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="MWBPPSVMGF3BY"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>');
   toggleSidePanel('#panel-donate'); return false; 
 });
 
